a04194838241e8bce2c3805a588878cb05dc0852
buschko <dennis.guck@gmail.com>
2014-08-02 17:30:17 +0200

    commited out SparseMatrix_free(ma); in main.cpp (Bug on Linux machine which needs to be fixed)

--- src/main.cpp
+++ src/main.cpp
@@ -848,10 +848,10 @@ int main(int argc, char* argv[]) {
 		SparseMatrixMEC_free(mecs);
         delete(mecs);
 	}
-	
-	SparseMatrix_free(ma);
-	
+
+	//SparseMatrix_free(ma);
+
 	delete(ma);
-    
+
 	return 0;
 }
